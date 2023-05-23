df1a02b70e0d961717416ca3280beea25918de5e
surani-hub <skmali1357@gmail.com>
2023-05-23 16:44:28 +0530

    first commit

---
+++ online-portal-app/src/main/java/com/onlineportal/app/portfolio_pkg/PortfolioWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.onlineportal.app.portfolio_pkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioWebsiteApplication.class, args);
+	}
+
+}
---
+++ online-portal-app/src/test/java/com/onlineportal/app/portfolio_pkg/PortfolioWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineportal.app.portfolio_pkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
